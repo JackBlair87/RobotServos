abfe775f16b3a63331164ac519a96406da569567
JackBlair87 <60362058+JackBlair87@users.noreply.github.com>
2020-08-03 17:51:39 -0400

    Add files via upload

---
+++ examples/moveForward.ino
@@ -0,0 +1,13 @@
+#include <RobotServos.h>
+
+RobotServos servos(11, 12);
+
+void setup() {
+  // put your setup code here, to run once:
+  servos.setState(1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
